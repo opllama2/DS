5a3277adf4139b64acaa9902ce1a047c4ad59b59
Ali Hassan <ali.hassan@insidetrack.com>
2019-12-01 14:58:51 +0200

    added an implementation of the Queue

---
+++ src/structs/queue/queue.ts
@@ -0,0 +1,29 @@
+import { DoublyLinkedList } from "../doubly-linked-list/doubly-linked-list";
+
+export class Queue {
+  private items: DoublyLinkedList = null;
+  constructor() {
+    this.items = new DoublyLinkedList();
+  }
+
+  public get size() {
+    return this.items.size;
+  }
+
+  /**
+   * Adds a value to the queue
+   * @param val the value to add to the queue
+   * @returns {number} the new size of the queue
+   */
+  public enqueue(val): number {
+    return this.items.push(val);
+  }
+
+  /**
+   * removes the next item in the queue
+   * @returns {any} the next item in the queue
+   */
+  public dequeue(): any {
+    return this.items.shift().value;
+  }
+}
