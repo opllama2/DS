b110bad273381a580a7a1a508711c0a671558ad5
Ali Hassan <ali.hassan@insidetrack.com>
2019-12-03 17:37:08 +0200

    fix: exposed BinaryHeap in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export * from "./structs/singly-linked-list/singly-linked-list";
 export * from "./structs/doubly-linked-list/doubly-linked-list";
 export * from "./structs/queue/queue";
 export * from "./structs/stack/stack";
+export * from "./structs/binary-heap/binary-heap";
